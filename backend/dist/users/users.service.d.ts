49ff4e5e0fa95ae56027d12f5caf90aa6966ec47
Ajit Singh <ajitsinghkaler0@gmail.com>
2025-03-16 09:10:37 +0530

    removed prod files

--- backend/dist/users/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'mongoose';
-import { UserDocument } from './user.schema';
-export declare class UsersService {
-    private userModel;
-    constructor(userModel: Model<UserDocument>);
-    create(createUserDto: any): Promise<UserDocument>;
-    findByEmail(email: string): Promise<UserDocument | null>;
-    findById(id: string): Promise<UserDocument | null>;
-}
